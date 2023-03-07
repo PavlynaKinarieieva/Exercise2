a752999ad24390fed4406ff89c4573a5bf891d62
Pavlyna Kinarieieva <pavlynakinarieieva@Air-Pavlyna.fritz.box>
2023-03-07 14:34:16 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        add();
+        subtract();
+        divide();
+        multiply();
+        }
+        public static void add() {
+           int number1 = 20;
+           int number2 = 10;
+
+           int result = number1 + number2;
+           System.out.println(result);
+        }
+        public static void subtract() {
+            int number1 = 20;
+            int number2 = 10;
+
+            int result = number1 - number2;
+            System.out.println(result);
+        }
+        public static void divide() {
+            int number1 = 20;
+            int number2 = 10;
+
+            int result = number1 / number2;
+            System.out.println(result);
+        }
+        public static void multiply() {
+            int number1 = 20;
+            int number2 = 10;
+
+            int result = number1 * number2;
+            System.out.println(result);
+    }
+}
